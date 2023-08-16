247ea0f36ec7da66d1848b6e77613f2034a64da1
sheikhazam <asheikh@hollandcollege.com>
2023-08-16 17:52:08 -0600

    init

--- server.js
+++ server.js
@@ -1,67 +1,31 @@
-const http = require('http');
 const WebSocket = require('ws');
 
-const server = http.createServer();
-const wss = new WebSocket.Server({ server });
+const server = new WebSocket.Server({ port: 8080 });
 
-const users = [
-    { id: 'user1', ws: null }, // Example users
-    { id: 'user2', ws: null },
-    { id: 'user3', ws: null }
-];
+const clients = new Set();
 
-wss.on('connection', (ws) => {
-    console.log('A client connected:', ws.id);
+server.on('connection', (client) => {
+    // Add the new client to the list
+    clients.add(client);
 
-    const user = users.find(u => u.ws === null);
-    if (user) {
-        user.ws = ws;
-        ws.id = user.id;
-        broadcastUsersList();
-    } else {
-        ws.close();
-    }
+    // Send a welcome message to the new client
+    client.send('Welcome to the chat!');
 
-    ws.on('message', (message) => {
-        try {
-            const messageObj = JSON.parse(message);
-            if (messageObj.type === 'message') {
-                const recipient = users.find(u => u.id === messageObj.targetUserId);
-                if (recipient && recipient.ws) {
-                    recipient.ws.send(JSON.stringify({
-                        type: 'message',
-                        content: messageObj.content,
-                        senderId: messageObj.senderId
-                    }));
-                }
-            }
-        } catch (error) {
-            console.error('Error processing message:', error);
-        }
+    // Broadcast messages to all connected clients
+    client.on('message', (message) => {
+        broadcast(message);
     });
 
-    ws.on('close', () => {
-        console.log('A client disconnected:', ws.id);
-        const user = users.find(u => u.id === ws.id);
-        if (user) {
-            user.ws = null;
-            broadcastUsersList();
-        }
+    // Remove the client from the list when they disconnect
+    client.on('close', () => {
+        clients.delete(client);
     });
-
-    broadcastUsersList();
 });
 
-function broadcastUsersList() {
-    const activeUsers = users.filter(u => u.ws !== null).map(u => ({ id: u.id }));
-    users.forEach(user => {
-        if (user.ws) {
-            user.ws.send(JSON.stringify({ type: 'users', data: activeUsers }));
+function broadcast(message) {
+    clients.forEach((client) => {
+        if (client.readyState === WebSocket.OPEN) {
+            client.send(message);
         }
     });
 }
-
-const PORT = 8089;
-server.listen(PORT, () => {
-    console.log(`WebSocket server is running on port ${PORT}`);
-});
